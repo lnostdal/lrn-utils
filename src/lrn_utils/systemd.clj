eeaf33992af2ad70a1c88c16f1cf8c03533edcc5
Lars Rune Nøstdal <larsnostdal@gmail.com>
2019-04-20 04:52:24 +0300

    systemd: this was actually what i released ..
    ..i think. #tired

--- src/lrn_utils/systemd.clj
+++ src/lrn_utils/systemd.clj
@@ -1,7 +1,8 @@
 (ns lrn-utils.systemd
   "This notifies systemd (look up `WatchdogSec`) that our process is alive."
   (:require [clojure.core.async :as async]
-            [lrn-utils.core :as core])
+            [lrn-utils.core :as core]
+            [taoensso.timbre :as log])
   (:import [info.faljse.SDNotify SDNotify]))
 
 
@@ -12,8 +13,12 @@
   [^long interval]
   (when (get (System/getenv) "NOTIFY_SOCKET")
     (async/go-loop []
-      (let [timeout-ch (async/timeout interval)]
-        (async/alt!
-          -dummy-ch- (throw (ex-info "This should never happen.." {}))
-          timeout-ch (SDNotify/sendWatchdog)))
+      (try
+        (let [timeout-ch (async/timeout interval)]
+          (async/alt!
+            -dummy-ch- (throw (ex-info "This should never happen.." {}))
+            timeout-ch (SDNotify/sendWatchdog)))
+        (catch Throwable ex
+          (log/fatal ex "systemd watchdog seems to be failing; this can't be good.")
+          (Thread/sleep 3000)))
       (recur))))
